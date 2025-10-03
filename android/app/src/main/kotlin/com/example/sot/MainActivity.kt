476a9a62cf16313d1892698a6a77334f0f8e2bdd
PC <daudraja185@gmail.com>
2025-10-03 10:29:56 -0700

    Backend Connected

--- android/app/src/main/kotlin/com/example/sot/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/sot/MainActivity.kt
@@ -1,49 +1,5 @@
-import android.content.pm.PackageManager
-import android.os.Bundle
+package com.example.sot
+
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
 
-class MainActivity : FlutterActivity() {
-    @override
-    fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
-            .setMethodCallHandler(
-                object : MethodCallHandler() {
-                    @Override
-                    fun onMethodCall(call: MethodCall, result: Result) {
-                        if (call.method.equals("getGoogleMapsApiKey")) {
-                            val apiKey: String? = this.apiKey
-                            result.success(apiKey)
-                        } else {
-                            result.notImplemented()
-                        }
-                    }
-                }
-            )
-    }
-
-    private val apiKey: String?
-        get() {
-            try {
-                val metaData: Bundle? = getPackageManager().getApplicationInfo(
-                    getPackageName(),
-                    PackageManager.GET_META_DATA
-                ).metaData
-                if (metaData != null) {
-                    return metaData.getString("com.google.android.geo.API_KEY")
-                }
-            } catch (e: PackageManager.NameNotFoundException) {
-                // Handle exception
-            }
-            return null
-        }
-
-    companion object {
-        private val CHANNEL: String = "com.sot/app"
-    }
-}
+class MainActivity : FlutterActivity()
